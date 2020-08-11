7a9013561432f26353bb5ab37c8bacf6cadfbb19
K.Guna Sree <49094537+gusre@users.noreply.github.com>
2020-08-11 22:15:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ app.config.update(
 	MAIL_SERVER='smtp.gmail.com',
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'gunasreesing@gmail.com',
-	MAIL_PASSWORD = 'gunaSREE1@'
+	MAIL_USERNAME = '',
+	MAIL_PASSWORD = ''
 	)
 mail = Mail(app)
 
